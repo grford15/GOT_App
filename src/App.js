de8aea8ee0a2ac7004efdf44c919a485b50a6585
Greg <g.rutherford10@gmail.com>
2020-01-09 17:51:45 +0000

    add functionality to retrieve api data

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Winter is Coming ...</h1>
-      </header>
-    </div>
-  );
+const BASE_URL = 'https://www.anapioficeandfire.com/api/characters';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      characters: [],
+      houses: [],
+      books: [],
+    };
+
+    this.fetchApiData = this.fetchApiData.bind(this);
+  }
+
+  fetchApiData() {
+    axios(`${BASE_URL}`)
+      .then(result => this.setState({ characters: result.data }))
+      .catch(error => error);
+  }
+
+  componentDidMount() {
+    this.fetchApiData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Winter is Coming ...</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
