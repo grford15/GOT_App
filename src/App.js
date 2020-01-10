439bac6776338ef20a18f54566601a0a49340804
Greg <g.rutherford10@gmail.com>
2020-01-10 14:50:11 +0000

    add router & components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Characters from './components/characters';
+import Books from './components/books';
+import Houses from './components/houses';
 import './App.css';
 
 const BASE_URL = 'https://www.anapioficeandfire.com/api/characters';
@@ -30,9 +39,35 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Winter is Coming ...</h1>
-        </header>
+        <Router>
+          <nav>
+            <div>
+              <Link to="characters">Characters</Link>
+            </div>
+            <div>
+              <Link to="houses">Houses</Link>
+            </div>
+            <div>
+              <Link to="books">Books</Link>
+            </div>
+          </nav>
+          <header className="App-header">
+            <h1>Winter is Coming ...</h1>
+          </header>
+          <div>
+            <Switch>
+              <Route exact path="/characters">
+                <Characters />
+              </Route>
+              <Route path="/houses">
+                <Houses />
+              </Route>
+              <Route path="/books">
+                <Books />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/components/houses.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Houses extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Houses Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Houses;
