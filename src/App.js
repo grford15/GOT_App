243690469f87398b97b1b6052d8bd63673677ef7
Greg <g.rutherford10@gmail.com>
2020-01-10 15:41:54 +0000

    character page now gathers data from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,31 +10,7 @@ import Books from './components/books';
 import Houses from './components/houses';
 import './App.css';
 
-const BASE_URL = 'https://www.anapioficeandfire.com/api/characters';
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      characters: [],
-      houses: [],
-      books: [],
-    };
-
-    this.fetchApiData = this.fetchApiData.bind(this);
-  }
-
-  fetchApiData() {
-    axios(`${BASE_URL}`)
-      .then(result => this.setState({ characters: result.data }))
-      .catch(error => error);
-  }
-
-  componentDidMount() {
-    this.fetchApiData();
-  }
-
   render() {
     return (
       <div className="App">
--- src/components/characters.js
+++ src/components/characters.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import Axios from 'axios';
 
 class Characters extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: [],
+    };
+  }
+
+  componentDidMount() {
+    Axios(
+      'https://www.anapioficeandfire.com/api/characters?page=1&pageSize=50',
+    )
+      .then(result => this.setState({ characters: result.data }))
+      .catch(error => error);
+  }
+
   render() {
+    const characters = this.state;
+    console.log(characters.characters);
+
     return (
       <div>
         <h1>Characters Page</h1>
+
+        {characters &&
+          characters.characters.map(item => (
+            <div>
+              <h4>{item.name}</h4>
+            </div>
+          ))}
       </div>
     );
   }
